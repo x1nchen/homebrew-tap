acb615386c1091a61dc5e6b0789d4a8a82040ad0
x1nchen <xinchenccx@gmail.com>
2021-06-11 15:36:57 +0800

    portainer-cli bump to 0.0.17

--- Formula/portainer-cli.rb
+++ Formula/portainer-cli.rb
@@ -1,8 +1,8 @@
 class PortainerCli < Formula
   desc "command line tool for portainer"
   homepage "https://github.com/x1nchen/portainer-cli"
-  url "https://github.com/x1nchen/portainer-cli/releases/download/v0.0.16/portainer-cli_0.0.16_darwin_amd64.tar.gz"
-  version "0.0.16"
+  url "https://github.com/x1nchen/portainer-cli/releases/download/v0.0.16/portainer-cli_0.0.17_darwin_amd64.tar.gz"
+  version "0.0.17"
   sha256 "eb0d2ce4a692a586cfa343d52aba25b232a2d8f02365703abad50089f24da6e2"
 
   def install
