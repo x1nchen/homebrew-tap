3aab88064685be4914d59920a7c4bed468d135fe
x1nchen <xinchenccx@gmail.com>
2021-07-08 14:35:26 +0800

    portainer-cli bump to 0.0.22

--- Formula/portainer-cli.rb
+++ Formula/portainer-cli.rb
@@ -1,10 +1,9 @@
 class PortainerCli < Formula
   desc "command line tool for portainer"
   homepage "https://github.com/x1nchen/portainer-cli"
-  url "https://github.com/x1nchen/portainer-cli/releases/download/v0.0.18/portainer-cli_0.0.18_darwin_amd64.tar.gz"
-  version "0.0.18"
-  sha256 "5c7c354bbc234f788e9788373be7e922eb6f74a1ef0d256bbbb1b1143fbbf13a"
-
+  url "https://github.com/x1nchen/portainer-cli/releases/download/v0.0.22/portainer-cli_0.0.22_darwin_amd64.tar.gz"
+  version "0.0.22"
+  sha256 "cd2fc466056e8a026e7f761a8ffd4c310b2bdf367315e654efc0e2feb765c3e0"
   def install
     bin.install "portainer-cli"
   end
